9407a4ae67cb3199e2c64d2a2c1b9e14c9c1b128
mktpriyamal <k.thisara@gmail.com>
2024-07-05 12:24:45 +0530

    updates 2024-07-05

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,25 +2,18 @@ const mysql = require('mysql');
 const { logger } = require('../utils/logger');
 const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = require('../utils/secrets');
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
+    connectionLimit: 10,  // Adjust based on your needs
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASS,
     database: DB_NAME
 });
 
-connection.connect((err) => {
-    if (err) {
-        logger.error(err.message);
-    } else {
-        logger.info('Database connected');
-    }
-});
-
 // Promisified query method
-connection.queryPromise = (sql, args) => {
+pool.queryPromise = (sql, args) => {
     return new Promise((resolve, reject) => {
-        connection.query(sql, args, (err, results) => {
+        pool.query(sql, args, (err, results) => {
             if (err) {
                 logger.error(err.message);
                 return reject(err);
@@ -30,12 +23,26 @@ connection.queryPromise = (sql, args) => {
     });
 };
 
+// Promisified getConnection method to handle transactions
+pool.getConnectionPromise = () => {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+            if (err) {
+                logger.error(err.message);
+                return reject(err);
+            }
+            resolve(connection);
+        });
+    });
+};
+
 // Promisified beginTransaction method
-connection.beginTransactionPromise = () => {
+pool.beginTransactionPromise = (connection) => {
     return new Promise((resolve, reject) => {
         connection.beginTransaction((err) => {
             if (err) {
                 logger.error(err.message);
+                connection.release();  // Release the connection back to the pool
                 return reject(err);
             }
             resolve();
@@ -44,25 +51,29 @@ connection.beginTransactionPromise = () => {
 };
 
 // Promisified commit method
-connection.commitPromise = () => {
+pool.commitPromise = (connection) => {
     return new Promise((resolve, reject) => {
         connection.commit((err) => {
             if (err) {
                 logger.error(err.message);
+                pool.rollbackPromise(connection)  // Rollback if commit fails
+                    .then(() => connection.release());
                 return reject(err);
             }
+            connection.release();
             resolve();
         });
     });
 };
 
 // Promisified rollback method
-connection.rollbackPromise = () => {
+pool.rollbackPromise = (connection) => {
     return new Promise((resolve) => {
         connection.rollback(() => {
+            connection.release();  // Release the connection back to the pool
             resolve();
         });
     });
 };
 
-module.exports = connection;
+module.exports = pool;
